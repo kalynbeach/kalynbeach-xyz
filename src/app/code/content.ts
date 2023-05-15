d823ac4ea04aea39eb79671fded14fc1bb86dff4
kalynbeach <kalynbeach@gmail.com>
2023-05-15 16:44:26 -0700

    add app/code/content.ts, use in Code page

---
+++ src/app/code/content.ts
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: 'kalynbeach-com',
+    description: '🌐 This site',
+    href: 'https://github.com/kalynbeach/kalynbeach-com'
+  },
+  {
+    title: 'wave-visions',
+    description: '🔊 Audio visualization experiments',
+    href: 'https://github.com/kalynbeach/wave-visions'
+  },
+  {
+    title: 'wave-dex',
+    description: '🗂️ Music project and library indexer',
+    href: 'https://github.com/kalynbeach/wave-dex'
+  }
+]
+
+export const jobs = [
+  {
+    company: 'CPR',
+    location: 'Orange, CA',
+    title: 'Contract Web Developer',
+    startDate: 'April 2015',
+    endDate: 'June 2015',
+    description: 'Freelance web development work'
+  },
+  {
+    company: 'Demand Media',
+    location: 'Santa Monica, CA',
+    title: 'Software Engineer Intern',
+    startDate: 'June 2015',
+    endDate: 'September 2015',
+    description: 'Interned on the eHow.com engineering team. Implemented frontend features and fixes with HTML, CSS, and JavaScript (with jQuery). First experiences with large codebases and sites with millions of visitors.'
+  },
+]
+
+const content = {
+  projects,
+  jobs
+}
+
+export default content
--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -1,43 +1,5 @@
+import { projects, jobs } from './content'
 import Card from '@/ui/Card'
-
-// TODO: Refactor data sources and data fetching
-
-const projects = [
-  {
-    title: 'kalynbeach-com',
-    description: '🌐 This site',
-    href: 'https://github.com/kalynbeach/kalynbeach-com'
-  },
-  {
-    title: 'wave-visions',
-    description: '🔊 Audio visualization experiments',
-    href: 'https://github.com/kalynbeach/wave-visions'
-  },
-  {
-    title: 'wave-dex',
-    description: '🗂️ Music project and library indexer',
-    href: 'https://github.com/kalynbeach/wave-dex'
-  }
-]
-
-const work = [
-  {
-    company: 'CPR',
-    location: 'Orange, CA',
-    title: 'Contract Web Developer',
-    startDate: 'April 2015',
-    endDate: 'June 2015',
-    description: 'Freelance web development work'
-  },
-  {
-    company: 'Demand Media',
-    location: 'Santa Monica, CA',
-    title: 'Software Engineer Intern',
-    startDate: 'June 2015',
-    endDate: 'September 2015',
-    description: 'Interned on the eHow.com engineering team. Implemented frontend features and fixes with HTML, CSS, and JavaScript (with jQuery). First experiences with large codebases and sites with millions of visitors.'
-  },
-]
 
 export default function Code() {
   return (
@@ -59,18 +21,16 @@ export default function Code() {
         <h2 className='mb-4 text-2xl font-heading tracking-wide'>Work</h2>
         <div className='flex flex-col gap-4'>
           {
-            work.map((job, i) => (
+            jobs.map((job, i) => (
               <div key={i} className='p-4 flex flex-col justify-between gap-2 font-mono border border-neutral-900 rounded-lg'>
                 {/* Job Heading */}
                 <div className='flex flex-row justify-between items-center'>
                   <h3 className='text-xl'>{job.title}</h3>
                   <h4 className='text-xl font-bold tracking-wide'>{job.company}</h4>
-                  {/* <span className='text-sm'>{job.startDate} - {job.endDate}</span> */}
                 </div>
                 {/* Job Info */}
                 <div className=''>
                   <div className='flex flex-row justify-between items-center'>
-                    {/* <h4 className='text-lg font-bold tracking-wide'>{job.company}</h4> */}
                     <span className='text-sm'>{job.startDate} - {job.endDate}</span>
                     <span className='text-sm'>{job.location}</span>
                   </div>
