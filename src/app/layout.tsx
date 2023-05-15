081c99589517941fbb6e0ea610b8625944e5fff9
kalynbeach <kalynbeach@gmail.com>
2023-05-14 18:45:10 -0700

    update global bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${ibm_plex_sans.variable} ${ibm_plex_mono.variable}`}>
+      <body className={`${inter.variable} ${ibm_plex_sans.variable} ${ibm_plex_mono.variable} bg-night-900`}>
         <div className='w-full md:max-w-3xl lg:max-w-4xl min-h-screen mx-auto flex flex-col'>
           <Header />
           <main className='min-h-max p-8 md:px-4 md:py-12 flex-1 flex flex-col'>
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -3,7 +3,7 @@ import Nav from './Nav'
 
 export default function Header() {
   return (
-    <header className='m-0 px-8 md:px-4 basis-32 flex flex-row justify-between items-center sticky top-0 bg-night-900 bg-transparent backdrop-blur z-50'>
+    <header className='m-0 px-8 md:px-4 basis-32 flex flex-row justify-between items-center sticky top-0 bg-transparent backdrop-blur z-50'>
       <Link href={'/'} className='font-mono md:text-lg text-white'>
         kalynbeach
       </Link>
