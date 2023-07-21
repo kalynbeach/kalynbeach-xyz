339cf97d2aafc29274b5204db416e8bb8246b249
kalynbeach <kalynbeach@gmail.com>
2023-07-20 18:25:07 -0700

    add src/lib/auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+//
+// Music Page Authentication
+//
+
+// TODO: Implement
+export function generatePassword() {}
+
+// Authenticate the user's page password input against the stored password hash
+export function authenticateUser(password: string): Boolean {
+  // TODO: Validate the page password against the stored password hash
+  // TODO: If the password is valid, return true
+  return true
+}
