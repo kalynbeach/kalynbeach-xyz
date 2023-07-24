f72e2648b037db68c8da0167d5ff1aa3bef0c62c
kalynbeach <kalynbeach@gmail.com>
2023-07-23 18:07:26 -0700

    reenable middleware to fix build

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,12 @@ import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 import { checkVerifiedCookie } from '@/lib/verification'
 
+export function middleware(request: NextRequest) {
+  // let cookie = request.cookies.get('verified')
+  // const isVerified = checkVerifiedCookie(request)
+  return NextResponse.next()
+}
 
-// export function middleware(request: NextRequest) {
-//   let cookie = request.cookies.get('verified')
-//   const isVerified = checkVerifiedCookie(request)
-//   return NextResponse.next()
-// }
-
-// export const config = {
-//   matcher: '/music/:path*'
-// }
+export const config = {
+  matcher: '/music/:path*'
+}
