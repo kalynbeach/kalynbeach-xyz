89fdfece847cafba4d52f944a6a73b98692c7d0e
kalynbeach <kalynbeach@gmail.com>
2023-09-28 01:19:32 -0700

    add svg social icons and use in Footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,30 +1,49 @@
+import Image from "next/image";
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
-
   return (
-    <footer className="w-full h-36 mx-auto my-0 px-8 md:px-4 flex flex-col md:flex-row justify-evenly md:justify-between items-start md:items-center font-mono text-xs">
+    <footer className="w-full h-48 md:h-36 mx-auto my-0 px-8 md:px-4 flex flex-col md:flex-row justify-evenly md:justify-between items-start md:items-center font-mono text-xs">
       <span className="">&copy; Kalyn Beach {currentYear}</span>
-      <div className="flex flex-row gap-12 justify-evenly items-center">
+      <div className="flex flex-row gap-12 justify-between items-center">
         <a
-          className="inline-block text-night-900 dark:text-white hover:text-kb-green-dark dark:hover:text-kb-green-light transition"
-          href="https://twitter.com/kalynbeach"
-          target="_blank"
-        >
-          X
-        </a>
-        <a
-          className="inline-block text-night-900 dark:text-white hover:text-kb-green-dark dark:hover:text-kb-green-light transition"
-          href="https://discordapp.com/channels/@me/kalynbeach/"
-          target="_blank"
-        >
-          Discord
-        </a>
-        <a
-          className="inline-block text-night-900 dark:text-white hover:text-kb-green-dark dark:hover:text-kb-green-light transition"
+          className="order-3 md:order-1"
           href="https://github.com/kalynbeach"
           target="_blank"
         >
-          GitHub
+          <Image
+            className="invert dark:invert-0"
+            src="/github.svg"
+            alt="GitHub"
+            width={17}
+            height={17}
+          />
+        </a>
+        <a
+          className="order-2"
+          href="https://discordapp.com/channels/@me/kalynbeach/"
+          target="_blank"
+        >
+          <Image
+            className="invert dark:invert-0"
+            src="/discord.svg"
+            alt="Discord"
+            width={16}
+            height={16}
+          />
+        </a>
+        <a
+          className="order-1 md:order-3"
+          href="https://x.com/kalynbeach"
+          target="_blank"
+        >
+          <Image
+            className="invert dark:invert-0"
+            src="/x.svg"
+            alt="X / Twitter"
+            width={15}
+            height={15}
+          />
         </a>
       </div>
     </footer>
