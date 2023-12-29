37c4549917b3f9792434fd3f56f38886f190cb80
kalynbeach <kalynbeach@gmail.com>
2023-12-28 20:14:42 -0800

    add and setup vercel/analytics again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import ThemeProvider from "@/components/theme-provider";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
