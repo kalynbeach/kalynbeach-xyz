20127367b530fc3edbbbe2a88a0376f952cd2492
kalynbeach <kalynbeach@gmail.com>
2024-01-18 18:04:45 -0800

    upgrade deps, temp patch lib/upload.ts

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,13 +1,14 @@
-import { put } from "@vercel/blob";
+// import { put } from "@vercel/blob";
 
-type Body = string | Blob | ArrayBuffer | FormData | ReadableStream<any> | File;
+// type Body = string | Blob | ArrayBuffer | FormData | ReadableStream<any> | File;
 
-export async function uploadBlob(filename: string, body: Body): Promise<string> {
-  console.log(`[uploadBlob] Uploading '${filename}'`);
-  const { url } = await put(filename, body, { access: 'public' });
-  console.log(`[uploadBlob] * Done - URL: '${url}'`);
-  return url;
-}
+// export async function uploadBlob(filename: string, body: Body): Promise<string> {
+//   console.log(`[uploadBlob] Uploading '${filename}'`);
+//   // const { url } = await put(filename, body, { access: 'public' });
+//   const blob = await put(filename, body, { access: "public" });
+//   console.log(`[uploadBlob] * Done - URL: '${url}'`);
+//   return url;
+// }
 
 export async function hello() {
   console.log("Hello from upload.ts");
